0f83c68373b38da54ecd34ff601453996403fa1e
hwangseojin <hsj6421@naver.com>
2023-10-09 21:10:16 +0900

    board practice

---
+++ board2/crud/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from .models import Student
+
+# Create your views here.
+def new(request):
+    return render(request, 'crud/new.html')
+
+def create(request):
+    student = Student()
+    student.name = request.GET['name']
+    student.age = request.GET['age']
+    student.major = request.GET['major']
+    student.description = request.GET['description']
+    student.save()
+    
+    return redirect('crud:detail', student.pk)
+
+def index(request):
+    students = Student.objects.all()
+    return render(request, 'crud/index.html', {
+        'students': students
+    })
+
+def detail(request, pk):
+    student = Student.objects.get(pk=pk)
+    return render(request, 'crud/detail.html',{
+        'student': student,
+    })
+
+def edit(request, pk):
+    student = Student.objects.get(pk=pk)
+    return render(request, 'crud/edit.html',{
+        'student': student,
+    })
+    
+def update(request, pk):
+    student = Student.objects.get(pk=pk)
+    student.name = request.GET['name']
+    student.age = request.GET['age']
+    student.major = request.GET['major']
+    student.description = request.GET['description']
+    student.save()
+    
+    return redirect('crud:detail', student.pk)
+
+def delete(request, pk):
+    student = Student.objects.get(pk=pk)
+    student.delete()
+    return redirect('crud:index')
