ca415c42e196a85296bf1f22ab36ccfa9d077567
hwangseojin223 <hsj6421@naver.com>
2024-06-04 16:34:37 +0900

    week15

--- algorithms/programmers/sql/즐겨찾기가 가장 많은 식당 정보 출력하기/sol.sql
+++ algorithms/programmers/sql/즐겨찾기가 가장 많은 식당 정보 출력하기/sol.sql
@@ -6,3 +6,14 @@ where (food_type, favorites) in (
     group by food_type
 )
 order by food_type desc
+
+-- sol2
+SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+FROM REST_INFO
+WHERE FAVORITES IN (
+    SELECT MAX(FAVORITES)
+    FROM REST_INFO 
+    GROUP BY FOOD_TYPE 
+    ORDER BY FAVORITES DESC)
+GROUP BY FOOD_TYPE
+ORDER BY FOOD_TYPE DESC
