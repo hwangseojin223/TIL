5e5ef3d337e89feb7424038408aeaaeb233ee1a3
hwangseojin223 <hsj6421@naver.com>
2024-12-31 11:44:01 +0900

    1230

--- algorithms/baekjoon/트리의 부모 찾기/sol.py
+++ algorithms/baekjoon/트리의 부모 찾기/sol.py
@@ -1,3 +1,4 @@
+""" recursionError
 node_cnt = int(input())
 matrix = [[] for _ in range(node_cnt+1)]
 
@@ -19,3 +20,28 @@ dfs(1)
 
 for i in range(2, node_cnt+1):
     print(visited[i])
+    
+ """ 
+import sys
+sys.setrecursionlimit(10**6)
+N = int(sys.stdin.readline())
+tree = [[] for _ in range(N + 1)]
+parent = [0] * (N + 1)
+
+for _ in range(N - 1):  # 간선의 갯수 = 노드 개수 - 1
+    a, b = map(int, sys.stdin.readline().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+
+def dfs(v):
+    for i in tree[v]:
+        if parent[i] == 0:
+            parent[i] = v
+            dfs(i)
+
+
+dfs(1)
+
+for j in range(2, len(parent)):
+    print(parent[j])
